39ed176571d43e318399d906d7f8c1539bfc50de
123 <qeqw>
2021-03-01 15:43:11 +0300

    header adaptive mobile
    mobile menu, socials
    change breakpoint logo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function() {
-
+    const menuButton = document.querySelector('.menu__button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    menuButton.addEventListener('click', () => {
+        console.log('clicked');
+        let expanded = menuButton.getAttribute('aria-expanded') === 'true' ? true :false ;
+        menuButton.setAttribute('aria-expanded', !expanded);
+        menuButton.classList.toggle('menu__button--open');
+        mobileMenu.classList.toggle('mobile-menu--open');
+    });
 })
